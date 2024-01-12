130e0447446c5de3abe5a35826e6d6c6236cfa61
lmsand <lmsand99@icloud.com>
2024-01-12 14:34:48 -0600

    intro and ui setup

---
+++ BarcodeScanner2/BarcodeScannerView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  BarcodeScanner2
+//
+//  Created by lexi sanders on 1/12/24.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
