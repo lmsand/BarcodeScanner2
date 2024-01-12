7b95286920edea2c07f2ba3d6a9acdfa125c47ef
lmsand <lmsand99@icloud.com>
2024-01-12 14:25:17 -0600

    Initial Commit

---
+++ BarcodeScanner2/BarcodeScanner2App.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanner2App.swift
+//  BarcodeScanner2
+//
+//  Created by lexi sanders on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanner2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScanner2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarcodeScanner2
+//
+//  Created by lexi sanders on 1/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
